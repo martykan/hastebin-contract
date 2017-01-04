152d92e91d80d83f108d61aec1be6af43fdc8321
Tomáš Martykán <t.martykan@gmail.com>
2017-01-04 18:10:42 +0100

    Commit code

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root." 1>&2
 else
-    cp -r HastebinContract /usr/share/
-    mv /usr/share/HastebinContract/hastebin.contract /usr/share/contractor/
-    chmod 755 /usr/share/HastebinContract/hastebin.py
-    chmod 644 /usr/share/contractor/hastebin.contract
+    cp -r TransfershContract /usr/share/
+    mv /usr/share/TransfershContract/transfersh.contract /usr/share/contractor/
+    chmod 755 /usr/share/TransfershContract/transfersh.py
+    chmod 644 /usr/share/contractor/transfersh.contract
 fi
 
